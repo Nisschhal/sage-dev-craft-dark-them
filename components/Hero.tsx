303aaffbda37869f0a4fb6313879bab27ceeb3e8
nischal puri <mrnischalpuri@gmail.com>
2025-01-15 10:19:02 +0545

    [hero animation] added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,18 @@
+"use client"
 import { FiArrowRight } from "react-icons/fi"
 import cursorIcon from "@/assets/images/cursor.png"
 import messageIcon from "@/assets/images/message.png"
 import Image from "next/image"
+import { motion } from "motion/react"
+import { useRef } from "react"
 export const Hero = () => {
+  const dragRef = useRef<HTMLDivElement>(null)
+
   return (
-    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-24 relative overflow-hidden">
+    <div
+      ref={dragRef}
+      className="bg-black w-full text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-24 relative overflow-hidden"
+    >
       <div className="absolute top-[calc(100%-120px)] w-[750px] h-[375px] sm:w-[1536] sm:h-[768] lg:w-[2800] lg:h-[1200] rounded-[100%] left-1/2 -translate-x-1/2 border border-[#b48cde] bg-[radial-gradient(closest-side,#000_95%,#9560EB)]"></div>
       <div className="container relative">
         <div className="flex justify-center items-center">
@@ -26,20 +34,35 @@ export const Hero = () => {
             <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center mt-8">
               One Task <br /> at a Time
             </h1>
-            <Image
-              src={cursorIcon}
-              alt="cursor"
-              width={200}
-              height={200}
+            {/* Curso dragable area */}
+            <motion.div
+              drag
+              dragConstraints={dragRef}
               className="hidden sm:block absolute top-[50%] right-[480px]"
-            />
-            <Image
-              src={messageIcon}
-              alt="cursor"
-              width={200}
-              height={200}
+            >
+              <Image
+                src={cursorIcon}
+                alt="cursor"
+                width={200}
+                height={200}
+                draggable="false" // drag with motion.div
+                className="max-w-none"
+              />
+            </motion.div>
+            <motion.div
+              drag
+              dragConstraints={dragRef}
               className="hidden sm:block  absolute top-[10%] left-[510px]"
-            />
+            >
+              <Image
+                src={messageIcon}
+                alt="cursor"
+                width={200}
+                height={200}
+                draggable="false"
+                className="max-w-none"
+              />
+            </motion.div>
           </div>
         </div>
         <div className="flex justify-center items-center">
